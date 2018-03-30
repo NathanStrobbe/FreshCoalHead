e72f85feaab4fd3f2a4d2ac00ba746bd73222683
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2018-03-30 22:30:06 +0200

    fix listener

--- src/main/java/fr/pinath/listener/BlockListener.java
+++ src/main/java/fr/pinath/listener/BlockListener.java
@@ -1,11 +1,13 @@
 package fr.pinath.listener;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
@@ -29,8 +31,13 @@ public class BlockListener implements Listener {
     public void onPlayerInteract(PlayerInteractEvent e) {
         if (buttons.contains(e.getClickedBlock())
                 && e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-            Bukkit.broadcastMessage("Hey wake up !");
+            Bukkit.broadcastMessage(ChatColor.DARK_RED + "Hey wake up !");
         }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        buttons.remove(e.getBlock());
     }
 
 }
