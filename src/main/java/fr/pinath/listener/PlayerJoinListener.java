7cdc15599579e5989f556ec15820168fccfe33b0
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2018-05-29 13:40:19 +0200

    remove the stored gui if the player quit the server

--- src/main/java/fr/pinath/listener/PlayerJoinListener.java
+++ src/main/java/fr/pinath/listener/PlayerJoinListener.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Map;
@@ -34,6 +35,20 @@ public class PlayerJoinListener implements Listener {
         if (e != null && e.getPlayer() != null) {
             if (!guiOfPlayers.containsKey(e.getPlayer())) {
                 guiOfPlayers.put(e.getPlayer(), new MainGUI(plugin, e.getPlayer()));
+            }
+        }
+    }
+
+    /**
+     * Remove useless GUI stored when a player left the server
+     *
+     * @param e event
+     */
+    @EventHandler
+    public void onPlayerLeft(PlayerQuitEvent e) {
+        if (e != null && e.getPlayer() != null) {
+            if (guiOfPlayers.containsKey(e.getPlayer())) {
+                guiOfPlayers.remove(e.getPlayer());
             }
         }
     }
